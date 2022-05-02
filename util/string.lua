4064bdb0906b157e602bacc39fe6a1ea8925be6f
someoneonsmile <someoneonsmile@gmail.com>
2022-05-03 00:07:39 +0800

    move corlorscheme autocommand to plugin config file and config the todo-comment hightlight to bg

--- util/string.lua
+++ util/string.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 M.not_blank = function(s)
-  return s and type(s) == "string" and vim.trim(s) ~= ''
+  return s and type(s) == 'string' and vim.trim(s) ~= ''
 end
 
 M.is_blank = function(s)
-  return s or type(s) == "string" and vim.trim(s) == ''
+  return s or type(s) == 'string' and vim.trim(s) == ''
 end
 
 return M
