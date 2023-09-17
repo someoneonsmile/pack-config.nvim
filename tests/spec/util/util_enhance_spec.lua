2799de1e203871f949d6f4c894bd587edf602b8e
someoneonsmile <someoneonsmile@gmail.com>
2023-09-18 01:58:47 +0800

    fix: parser test

--- tests/spec/util/util_enhance_spec.lua
+++ tests/spec/util/util_enhance_spec.lua
@@ -1,19 +1,14 @@
 local enhance = require('pack-config.util.enhance')
-local eq = assert.same
-local neq = assert.are_not.same
 
 describe('enhance test', function()
   it('load env', function()
-    local run = function(r)
-      return r()
-    end
     local content = 'print("foo:",foo) return function () if foo == "bar" then return 1 else return 0 end end'
     local env = setmetatable({ foo = 'bar' }, { __index = _G })
     local func, err = assert(load(content, 'abc', 'bt', env))
     if err ~= nil then
       print(err)
     end
-    eq(1, func()())
+    assert.same(1, func()())
   end)
   it('dofile env', function()
     local env = { foo = 'bar' }
@@ -23,6 +18,6 @@ describe('enhance test', function()
     file:write(content)
     file:close()
     local ok, func = enhance.dofile(tmpname, 'bt', env)
-    eq('bar', func())
+    assert.same('bar', func())
   end)
 end)
