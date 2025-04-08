c0597631f89f7666f256515c3a767ca7769d2a49
someoneonsmile <someoneonsmile@gmail.com>
2025-04-08 17:23:14 +0800

    chore: add test for context

---
+++ tests/spec/context_spec.lua
@@ -0,0 +1,21 @@
+local Context = require('pack-config.context')
+
+describe('context test', function()
+  -- ----------------------------------------------------------------------
+  --    - util.id -
+  -- ----------------------------------------------------------------------
+
+  it('context pair', function()
+    local a = Context:new('a')
+    local i = 0
+    local paired_private = false
+    for key, value in pairs(a) do
+      i = i + 1
+      if key ~= nil and string.match(key, '^_') then
+        paired_private = true
+      end
+    end
+    assert.same(0, i)
+    assert.same(false, paired_private)
+  end)
+end)
