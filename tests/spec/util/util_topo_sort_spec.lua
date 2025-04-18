b3abd3c092e8a714ecdb3188e33c3e788db22546
someoneonsmile <someoneonsmile@gmail.com>
2025-04-18 10:50:44 +0800

    fix: util_topo_sort_spec

--- tests/spec/util/util_topo_sort_spec.lua
+++ tests/spec/util/util_topo_sort_spec.lua
@@ -23,6 +23,17 @@ describe('util topo_sort test', function()
       sorter:sort { { '1' }, { '2', after = { '3' } }, { '3' } }
     )
   end)
+  it('util topo_sort dependency before, keep order', function()
+    local sorter = topo_sort:new(function(_, v)
+      return v[1]
+    end, function(v)
+      return fn.with_default {}(v.after)
+    end)
+    assert.same(
+      { { '1' }, { '2' }, { '3', after = { '2' } }, { '4' }, { '5' } },
+      sorter:sort { { '1' }, { '2' }, { '3', after = { '2' } }, { '4' }, { '5' } }
+    )
+  end)
   it('util topo_sort simple dependency, but more items', function()
     local sorter = topo_sort:new(function(_, v)
       return v[1]
